b896e094ccaf7157c475029af883596ffd0b8add
Alex <alxsiren@gmail.com>
2020-01-05 17:24:54 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+//'use strict'
+var leftBorderWidth = 1;
+let second = 2;
+const pi = 3.12;
+let person = {
+    name: "John",
+    age: 25,
+    isMarried: false
+}
+console.log(person.age);
+//alert('Hello world');
+let answer = confirm("Are you here?");
+console.log(answer);
+let secondAnswer = prompt("Есть ли Вам 18?", "Да");
+console.log(secondAnswer);
+console.log("Type =" + typeof(secondAnswer));
